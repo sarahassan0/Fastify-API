0b5caadd665d0c09f7872a1ee4cf5ecae6a75b87
Sarah hassan <sarraree5@gmail.com>
2024-05-16 10:08:09 +0300

    chore: change db url

--- API/db.js
+++ API/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/fastify-app");
+    await mongoose.connect(process.env.DB_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
