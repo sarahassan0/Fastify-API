a9ed399cfef78a72d57b498299b7e7a32174fc27
Sarah hassan <sarraree5@gmail.com>
2024-05-16 09:05:35 +0300

    docs: integrate  swagger Documentation for API's endpoints

--- API/app.js
+++ API/app.js
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
 import connectDB from "./db.js"; 
 import userRoutes from "./routes/userRoutes.js";
 
@@ -11,6 +12,14 @@ app.register(cors, {
   methods: ["GET", "PUT", "POST", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
+});
+
+app.register(fastifySwagger, {
+  exposeRoute: true,
+  routePrefix: "/docs",
+  swagger: {
+    info: { title: "Fastify App Documentation", version: "1.0.0" },
+  },
 });
 
 connectDB(); 
