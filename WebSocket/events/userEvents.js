d1b809844496f734153c9477bcc92ead0fb6af35
Sarah hassan <sarraree5@gmail.com>
2024-05-16 22:53:01 +0300

    feat: implemen updateUser, deleteUser, getUsers events

--- WebSocket/events/userEvents.js
+++ WebSocket/events/userEvents.js
@@ -1,5 +1,9 @@
 import UserService from "../../API/services/userService.js";
+import userAuthSocket from "../../middlewares/userAuthSocket.js";
 
+async function say_hi(ws, data) {
+  ws.send(JSON.stringify({ event: "say-hiSuccess", data: "Hi!" }));
+}
 async function signupUser(ws, data) {
   try {
     if (!data.name || !data.email || !data.password) {
@@ -54,8 +58,44 @@ async function getUser(ws, data) {
   }
 }
 
+async function updateUser(ws, data) {
+  try {
+    const authenticatedUser = await userAuthSocket(ws, data.token, data.userId);
+
+    const { userId, ...userData } = data;
+    const updatedUser = await UserService.updateUser(userId, userData);
+    ws.send(JSON.stringify({ event: "updateUserSuccess", data: updatedUser }));
+  } catch (error) {
+    ws.send(JSON.stringify({ event: "updateUserError", error: error.message }));
+  }
+}
+
+async function deleteUser(ws, data) {
+  try {
+    const authenticatedUser = await userAuthSocket(ws, data.token, data.userId);
+
+    await UserService.deleteUser(data.userId);
+    ws.send(JSON.stringify({ event: "deleteUserSuccess" }));
+  } catch (error) {
+    ws.send(JSON.stringify({ event: "deleteUserError", error: error.message }));
+  }
+}
+
+async function getUsers(ws, _) {
+  try {
+    const users = await UserService.getUsers();
+    ws.send(JSON.stringify({ event: "getUsersSuccess", data: users }));
+  } catch (error) {
+    ws.send(JSON.stringify({ event: "getUsersError", error: error.message }));
+  }
+}
+
 export default {
+  say_hi,
   signupUser,
   loginUser,
-  getUser
+  getUser,
+  updateUser,
+  deleteUser,
+  getUsers,
 };
--- WebSocket/sokets/userSocket.js
+++ WebSocket/sokets/userSocket.js
@@ -1,14 +1,19 @@
 import userEvents from "./../events/userEvents.js";
+import notFoundEventHandler from "../utils/NotFoundEventHandler.js";
 
 export default async function (ws, message) {
   const { event, data } = JSON.parse(message);
   const events = {
+    say_hi: userEvents.say_hi,
     signupUser: userEvents.signupUser,
     loginUser: userEvents.loginUser,
     getUser: userEvents.getUser,
+    updateUser: userEvents.updateUser,
+    deleteUser: userEvents.deleteUser,
+    getUsers: userEvents.getUsers,
   };
   if (!events[event]) {
-   console.log("Event not found");
+    await notFoundEventHandler(ws);
     return;
   }
   await events[event](ws, data);
